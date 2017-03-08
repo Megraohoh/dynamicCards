37f9860cc81c62e244ee1c82a3ef5b53c34928bb
Meghan Debity <mdebity@gmail.com>
2017-03-07 20:35:07 -0600

    throwing in basic code

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+//***************************************************
+// CREATE CARD
+//***************************************************
+// When user enters text into <textarea> box 
+// clicks <Create>, function creates a new card
+// (pushes <userInput> text string for new Card to cardArray)
+// then writes the cardHTML to the DOM
+//
+// Displays <alert> to user if clicks <Create> button
+// without entering any text
+//***************************************************
