55f1dd6e1787920fcfe62069dda20045a0ee2772
Meghan Debity <mdebity@gmail.com>
2017-03-27 19:40:29 -0500

    breaking things

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
-var containerDiv = document.getElementById("container");
+var cardArea = document.getElementById("cardArea");
 var inputArea = document.getElementById("inputArea");
 var createBtn = document.getElementById("createBtn");
+var deleteBtn = document.getElementById("deleteBtn");
 var identifier = 0;
 
-function writeToDom(){
-	identifier += 1;
-	var createCard = "";
 
-	createCard += `<div class="cardArea" id="newCard-${identifier}">`;
-	createCard += `<h3>Oh look, your card!!</h3>`;
-	createCard += `<img src="http://cdn.someecards.com/someecards/usercards/programmer-n-proh-gram-er-an-organism-that-turns-caffeine-and-pizza-into-software-8a828.png" alt="someecards programmer joke">`;
-	createCard += `<h3> ${inputArea.value}</h3>`;
-	createCard += `<button class="btn" id="deleteBtn">Delete</button>`;
-	createCard += `</div>`;
-	containerDiv.innerHTML += createCard;
+function writeToDom(){
+	if(inputArea !=="")	{
+		var createCard = "";
+		createCard += `<div class="cardArea" id="newCard-${identifier}">`;
+		createCard += `<h3>Oh look, your card!!</h3>`;
+		createCard += `<img src="http://cdn.someecards.com/someecards/usercards/programmer-n-proh-gram-er-an-organism-that-turns-caffeine-and-pizza-into-software-8a828.png" alt="someecards programmer joke">`;
+		createCard += `<h3> ${inputArea.value}</h3>`;
+		createCard += `<button class="btn deleteBtn">Delete</button>`;
+		createCard += `</div>`;
+		cardArea.innerHTML += createCard;
+		identifier += 1;
+	} else {
+		alert("Hey! Add some input to generate the card!")
+	}	
 }
 
 createBtn.addEventListener("click", writeToDom);
 
-document.body.addEventListener("click", function(e) {
-	if (event.target.className === "deleteBtn"){
+deleteBtn.addEventListener("click", deleteCard);
+
+function deleteCard(e) {
+// console.log("working", deleteCard);
+	if (event.target.classList === "deleteBtn"){
 		event.target.parentNode.remove();	
 	}
 
-});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
