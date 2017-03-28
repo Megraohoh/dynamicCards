86efb2b583f753f4573f58fbfb66ee5c4d4f935b
Meghan Debity <mdebity@gmail.com>
2017-03-27 20:39:32 -0500

    working

--- main.js
+++ main.js
@@ -12,16 +12,25 @@ function writeToDom(){
 		createCard += `<h3>Oh look, your card!!</h3>`;
 		createCard += `<img src="http://cdn.someecards.com/someecards/usercards/programmer-n-proh-gram-er-an-organism-that-turns-caffeine-and-pizza-into-software-8a828.png" alt="someecards programmer joke">`;
 		createCard += `<h3> ${inputArea.value}</h3>`;
-		createCard += `<button class="btn deleteBtn">Delete</button>`;
+		createCard += `<button class="btn" id="deleteBtn-${counter}">Delete</button>`;
 		createCard += `</div>`;
 		newCard.innerHTML += createCard;
 		counter ++;
+
 		
-	console.log("hello");
+}
+
+
+
+function cardBeGone(){
+ 	if (event.target.id.includes("deleteBtn")){
+ 		// console.log("yo", event.target.parentNode);
+ 		newCard.removeChild(event.target.parentNode);	
+ 	}
 };
 
 createBtn.addEventListener("click", writeToDom);
-
+newCard.addEventListener("click", cardBeGone);
 
 
 
